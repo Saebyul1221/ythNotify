0929a09d904de509f1a5313ce29a07216d586153
CwhiteKJ <dw696969@naver.com>
2021-06-04 17:45:54 +0900

    test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,23 @@
-const client = require("../src/index.js")
-const test = new client.Client({
-  token: "T O K E N",
-  youtubers: ["A", "B"],
-  streamers: "WhiteKJ",
+const yth = require("../src/index.js")
+// Draft Code
+
+// const test = new client.Client({
+//   token: "T O K E N",
+//   youtubers: ["A", "B"],
+//   streamers: "WhiteKJ",
+// })
+
+const test = new yth.Client({
+  youtube: {
+    token: "T O K E N",
+    youtubers: ["A", "B"],
+  },
+  twitch: {
+    secreat: "SUPER SECREAT KEY",
+    clientid: "1234",
+    streamers: "WhiteKJ",
+  },
+  interval: 20,
 })
 
 test.on("YoutubeUpload", (a) => console.log(a))
