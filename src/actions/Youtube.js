47828d30aa5eea810fe30a6ff79a938fe7581341
CwhiteKJ <dw696969@naver.com>
2021-06-04 21:08:35 +0900

    feat: add youtube event

--- src/actions/Youtube.js
+++ src/actions/Youtube.js
@@ -1,10 +1,70 @@
 "use strict"
 
+const Parser = require("rss-parser")
+const parser = new Parser()
+const Youtube = require("simple-youtube-api")
+const startAt = Date.now()
+const lastVideos = {}
+
 class YoutubeUpload {
   constructor(client) {
-    console.log("Youtube.js")
-    setImmediate(function () {
-      client.emit("YoutubeUpload", "Foo")
+    this.client = client
+    this.youtube = new Youtube(this.client._options?.youtube.token)
+
+    this.check()
+    setInterval(() => this.check(), this.client?._interval * 1000)
+  }
+
+  async getLastVideo(_, rssURL) {
+    let content = await parser.parseURL(rssURL)
+    let tLastVideos = content.items.sort((a, b) => {
+      let aPubDate = new Date(a.pubDate || 0).getTime()
+      let bPubDate = new Date(b.pubDate || 0).getTime()
+      return bPubDate - aPubDate
+    })
+    return tLastVideos[0]
+  }
+
+  async checkVideos(youtubeChannelName, rssURL) {
+    let lastVideo = await this.getLastVideo(youtubeChannelName, rssURL)
+    if (!lastVideo) return
+    if (new Date(lastVideo.pubDate).getTime() < startAt) return
+    let lastSavedVideo = lastVideos[youtubeChannelName]
+    if (lastSavedVideo && lastSavedVideo.id === lastVideo.id) return
+    return lastVideo
+  }
+
+  getYoutubeChannelIdFromURL(url) {
+    let id = null
+    url = url.replace(/(>|<)/gi, "").split(/(\/channel\/|\/user\/)/)
+    if (url[2]) id = url[2].split(/[^0-9a-z_-]/i)[0]
+    return id
+  }
+
+  async getYoutubeChannelInfos(name) {
+    let channel = null
+    let id = this.getYoutubeChannelIdFromURL(name)
+    if (id) {
+      channel = await this.youtube.getChannelByID(id)
+    }
+    if (!channel) {
+      let channels = await this.youtube.searchChannels(name)
+      if (channels.length > 0) channel = channels[0]
+    }
+
+    return channel
+  }
+  async check() {
+    this.client._youtubers.forEach(async (youtuber) => {
+      let channelInfos = await this.getYoutubeChannelInfos(youtuber)
+      if (!channelInfos) return
+      let video = await this.checkVideos(
+        channelInfos.raw.snippet.title,
+        "https://www.youtube.com/feeds/videos.xml?channel_id=" + channelInfos.id
+      )
+      if (!video) return
+      setImmediate(() => this.client.emit("YoutubeUpload", video))
+      lastVideos[channelInfos.raw.snippet.title] = video
     })
   }
 }
--- test/test.js
+++ test/test.js
@@ -1,23 +1,17 @@
-const yth = require("../src/index.js")
-// Draft Code
-
-// const test = new client.Client({
-//   token: "T O K E N",
-//   youtubers: ["A", "B"],
-//   streamers: "WhiteKJ",
-// })
-
-const test = new yth.Client({
+const Yth = require("../src/index.js")
+const yth = new Yth.Client({
   youtube: {
-    token: "T O K E N",
-    youtubers: ["A", "B"],
+    token: "token of youtube api",
+    youtubers: ["https://www.youtube.com/channel/UCG1Q1vaN5Kg-nmd4e3iJzPA"],
   },
   twitch: {
     secreat: "SUPER SECREAT KEY",
     clientid: "1234",
-    streamers: "WhiteKJ",
+    streamers: "https://www.twitch.tv/woowakgood",
   },
   interval: 20,
 })
 
-test.on("YoutubeUpload", (a) => console.log(a))
+yth.on("YoutubeUpload", (data) => {
+  console.log("New video detected! \n", data)
+})
